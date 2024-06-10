a730ef31fb2d87e4c084f257a3529710b8798fb8
Gaeun <rkdms1111111@naver.com>
2024-06-10 18:07:11 +0900

    url 변경

--- src/main/java/com/sparta/icy/controller/NewsfeedController.java
+++ src/main/java/com/sparta/icy/controller/NewsfeedController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/boards")
+@RequestMapping("/newsfeed")
 public class NewsfeedController {
     private final NewsfeedService newsfeedService;
 
