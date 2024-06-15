e4a72ba1ec8ffd82aa07a11b8aee85157ad7f77e
fenec_fox <atimevil@gmail.com>
2024-06-15 20:27:45 +0900

    # test : 유저 정보 조회 기능 성공 테스트

---
+++ src/test/java/com/sparta/icy/UserControllerTest.java
@@ -0,0 +1,62 @@
+package com.sparta.icy.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.icy.dto.UserProfileResponse;
+import com.sparta.icy.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController)
+                .build();
+        objectMapper = new ObjectMapper();
+    }
+
+
+    @Test
+    void getUserTest() throws Exception {
+        // given
+        long userId = 1L;
+        UserProfileResponse userProfileResponse = new UserProfileResponse("testuser", "nickname", "testuser@example.com", "intro");
+        when(userService.getUser(userId)).thenReturn(userProfileResponse);
+
+        // when
+        mockMvc.perform(get("/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value("testuser"))
+                .andExpect(jsonPath("$.nickname").value("nickname"))
+                .andExpect(jsonPath("$.email").value("testuser@example.com"))
+                .andExpect(jsonPath("$.intro").value("intro"))
+                .andDo(print());
+
+        // then
+        verify(userService, times(1)).getUser(userId);
+    }
+}
