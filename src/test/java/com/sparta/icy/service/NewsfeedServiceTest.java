5b105c3849b9587b4b8626f6d2df21c4f4e36790
fenec_fox <atimevil@gmail.com>
2024-06-16 21:30:20 +0900

    test init

---
+++ src/test/java/com/sparta/icy/service/NewsfeedServiceTest.java
@@ -0,0 +1,28 @@
+package com.sparta.icy.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsfeedServiceTest {
+
+    @Test
+    void createNewsfeed() {
+    }
+
+    @Test
+    void getNewsfeed() {
+    }
+
+    @Test
+    void updateNewsfeed() {
+    }
+
+    @Test
+    void deleteNewsfeed() {
+    }
+
+    @Test
+    void getAllNewsfeed() {
+    }
+}
