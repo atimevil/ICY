ff4f3a24b7fbdbaf0e26e986d9d67234e682c769
fenec_fox <atimevil@gmail.com>
2024-06-16 22:57:16 +0900

    test init

---
+++ src/test/java/com/sparta/icy/controller/NewsfeedControllerTest.java
@@ -0,0 +1,28 @@
+package com.sparta.icy.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsfeedControllerTest {
+
+    @Test
+    void createNewsfeed() {
+    }
+
+    @Test
+    void getNewsfeed() {
+    }
+
+    @Test
+    void updateNewsfeed() {
+    }
+
+    @Test
+    void deleteNewsfeed() {
+    }
+
+    @Test
+    void getAllNewsfeed() {
+    }
+}
